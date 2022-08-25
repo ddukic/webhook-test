a1b6fc252c346279cf07c7a102f40f085fee1cf3
Dane Dukic <ddukic86@gmail.com>
2022-08-25 12:42:12 +0200

    Add upload artifact action

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,9 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
-    },
+    defaultCommandTimeout: 500,
+    retries: { runMode: 1, openMode: 1 },
+
+    setupNodeEvents(on, config) {},
   },
-})
+});
---
+++ cypress/e2e/1-getting-started/task1.cy.js
@@ -0,0 +1,18 @@
+describe("Random tests", () => {
+  let i = 0;
+  before(() => {
+    // cy.get("I dont exist");
+  });
+  beforeEach(() => {
+    cy.log("Before each");
+    i++;
+  });
+  it("First test", () => {
+    cy.log("Im running");
+    if (i === 2) {
+      cy.log("run #2");
+    } else {
+      cy.get("I dont exist");
+    }
+  });
+});
