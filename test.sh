882d4c81a98f644025253deb1f55d799f9a08a4b
Dane Dukic <ddukic86@gmail.com>
2022-12-20 13:02:04 +0100

    Add read json

--- test.sh
+++ test.sh
@@ -1,1 +1,21 @@
-echo $(cat ./results/results.json | jq '.totalTests')
+
+for arg; do
+    search_dir="results $arg"
+    for entry in "$search_dir"/* 
+    do
+        totalTests=$(($totalTests+$(cat ./"$entry" | jq '.totalTests')))
+        totalPassed=$(($totalPassed+$(cat ./"$entry" | jq '.totalPassed')))
+        totalFailed=$(($totalFailed+$(cat ./"$entry" | jq '.totalFailed')))
+        totalSkipped=$(($totalSkipped+$(cat ./"$entry" | jq '.totalSkipped')))
+    done
+done
+
+JSON_STRING=$( jq -n \
+                  --arg totalTests "$totalTests" \
+                  --arg totalPassed "$totalPassed" \
+                  --arg totalFailed "$totalFailed" \
+                  --arg totalSkipped "$totalSkipped" \
+                  '$ARGS.named' )
+
+echo $JSON_STRING > test_run.json
+
